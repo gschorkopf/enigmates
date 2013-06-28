445e6017fa5a4161379238c42eab6c5c60519987
Geoff Schorkopf <gschorkopf@gmail.com>
2013-06-28 11:18:07 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Enigmates::Application.config.secret_token = '1efa30a100f47c149ba4bfde985a979001a6da3cb96fcc28feba88267b46ff5b9bd1e5947b63acc083b27ce54ff7f417fc193a169f689067b3e8e77c3d2ddc54'
