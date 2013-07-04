133d2e22ac67451ca79845364cd19a5bc5e80b1d
Geoff Schorkopf <gschorkopf@gmail.com>
2013-07-03 21:59:44 -0600

    Starter seed data

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130704030341) do
+ActiveRecord::Schema.define(:version => 20130704035229) do
 
   create_table "attempts", :force => true do |t|
     t.integer  "puzzle_id"
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(:version => 20130704030341) do
   create_table "puzzles", :force => true do |t|
     t.string   "name"
     t.string   "topic"
-    t.string   "type"
+    t.string   "format"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
