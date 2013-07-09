dfa0ceba110a288e008af5120439d82a639d748d
Geoff Schorkopf <gschorkopf@gmail.com>
2013-07-08 21:37:17 -0600

    Added environment variables for omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,13 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['TWITTER_ENIGMATES_ID'], ENV['TWITTER_ENIGMATES_SECRET']
-  provider :facebook, ENV['FB_ENIGMATES_ID'], ENV['FB_ENIGMATES_SECRET']
-  provider :google_oauth2, ENV["GOOGLE_ENIGMATES_ID"], ENV["GOOGLE_ENIGMATES_SECRET"]
+if Rails.env.production?  
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :twitter, ENV['TWITTER_ENIGMATES_ID_production'], ENV['TWITTER_ENIGMATES_SECRET_production']
+    provider :facebook, ENV['FB_ENIGMATES_ID_production'], ENV['FB_ENIGMATES_SECRET_production']
+    provider :google_oauth2, ENV["GOOGLE_ENIGMATES_ID_production"], ENV["GOOGLE_ENIGMATES_SECRET_production"]
+  end
+else
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :twitter, ENV['TWITTER_ENIGMATES_ID_development'], ENV['TWITTER_ENIGMATES_SECRET_development']
+    provider :facebook, ENV['FB_ENIGMATES_ID_development'], ENV['FB_ENIGMATES_SECRET_development']
+    provider :google_oauth2, ENV["GOOGLE_ENIGMATES_ID_development"], ENV["GOOGLE_ENIGMATES_SECRET_development"]
+  end
 end
