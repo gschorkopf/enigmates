a7f15cb8348e241047fb3e1f168858aab1c8c480
Geoff Schorkopf <gschorkopf@gmail.com>
2013-07-16 20:23:15 -0600

    Amended invites, added status to separate ideas

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -3,23 +3,25 @@ class InvitesController < ApplicationController
     @attempt = Attempt.find(params[:attempt_id])
     @user = User.find_by_name(params[:name])
 
-    if @user && @attempt.within_player_limit?
-      if @user.invited?(@attempt.id, current_user.id)
-        Invite.where(
-          attempt_id: @attempt.id,
-          sender_id: current_user.id).first.mark_as_unread
-        @warning = "#{current_user} has re-sent an invitation to #{@user}!"
+    if @user
+      if @attempt.within_player_limit?
+        if @user.invited?(@attempt.id, current_user.id)
+          Invite.where(
+            attempt_id: @attempt.id,
+            sender_id: current_user.id).first.mark_as_unread
+          @warning = "#{current_user} has re-sent an invitation to #{@user}!"; @status = :repeat
+        else
+          @attempt.add_player(@user.id)
+          @invite = Invite.create(
+            sender_id: current_user.id,
+            receiver_id: @user.id,
+            puzzle_id: @attempt.puzzle.id,
+            attempt_id: @attempt.id)
+          @message = "#{current_user} has sent an invitation to #{@user}!"; @status = :first
+        end
       else
-        @attempt.add_player(@user.id)
-        @invite = Invite.create(
-          sender_id: current_user.id,
-          receiver_id: @user.id,
-          puzzle_id: @attempt.puzzle.id,
-          attempt_id: @attempt.id)
-        @message = "#{current_user} has sent an invitation to #{@user}!"
+        @warning = "Player limit (4) has been reached!"; @status = :limit
       end
-    else
-      @warning = "Player limit (4) has been reached!"
     end
   end
 end
